aca82ec52cd52c826cfc98c9784e4d9156231a5c
Kamyar John Salahi <kamyar@myth60.stanford.edu>
2024-04-28 14:46:29 -0700

    Adding sysnames

--- main.cpp
+++ main.cpp
@@ -1,25 +1,40 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <sys/user.h>
+#include "sysnames.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        exit(0);
+    }
+
+    char* chargs[argc];
+    int i = 0;
+
+    while (i < argc - 1) {
+        chargs[i] = argv[i+1];
+        i++;
+    }
+
     pid_t child_pid = fork();
+    chargs[i] = NULL;
     if (child_pid == -1) {
         // Error handling
-        std::cerr << "Failed to fork\n";
+        fprintf(stderr, "Failed to fork");
         return 1;
     } else if (child_pid == 0) {
         // Child process
         ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
-        execl("/bin/ping", "ping", "8.8.8.8", nullptr); // Change IP address as needed
+        execvp(chargs[0], chargs);
     } else {
         // Parent process
         int status;
         waitpid(child_pid, &status, 0);
         if (WIFEXITED(status)) {
-            std::cout << "Child process exited normally\n";
+            printf("Child process exited normally");
             return 0;
         }
 
@@ -29,12 +44,16 @@ int main() {
             if (WIFEXITED(status)) {
                 break;
             }
-
+            
             // Check if syscall was made
             struct user_regs_struct regs;
             ptrace(PTRACE_GETREGS, child_pid, nullptr, &regs);
-            std::cout << "Syscall number: " << regs.orig_rax << "\n"; // The syscall number is in register x8
+            long syscall_num = regs.orig_rax;
+            fprintf(stderr, "system call number %ld name %s from pid %d\n", syscall_num, callname(syscall_num), child_pid);
         }
     }
     return 0;
 }
+
+
+
