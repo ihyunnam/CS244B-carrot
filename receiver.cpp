869ad2a716880fbfd305f892b5342269567e85d0
Chris Pondoc <chris.pondoc@comcast.net>
2024-05-30 21:52:13 +0000

    Initial work on receiver

--- receiver.cpp
+++ receiver.cpp
@@ -4,8 +4,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define PORT 12345 // Change the port number here
+// Code for protobufs
+#include "protobufs/messages/message.pb.h"
+#include "protobufs/messages/message.pb.cc"
+
+#define PORT 12346 // Change the port number here
 #define MAX_BUFFER_SIZE 1024
+using namespace std;
 
 int main() {
     int sockfd;
@@ -40,7 +45,29 @@ int main() {
     while (true) {
         n = recvfrom(sockfd, buffer, MAX_BUFFER_SIZE, 0, reinterpret_cast<sockaddr*>(&cliaddr), &len);
         buffer[n] = '\0';
-        std::cout << "Received message from client: " << buffer << std::endl;
+
+        // Deserialize message
+        CarrotMessage deserialized_message;
+        string serialized_data = buffer;
+        deserialized_message.ParseFromString(serialized_data);
+
+        // Print message data
+        cout << "Message: " << deserialized_message.message() << endl;
+        cout << "To Address: " << deserialized_message.ip_address().c_str() << endl;
+        cout << "To Port: " <<  htons(deserialized_message.port()) << endl;
+
+        // Set up destination info
+        struct sockaddr_in dest_addr;
+        dest_addr.sin_family = AF_INET;
+
+        // Get destination information from the deserialized string
+        dest_addr.sin_port = htons(deserialized_message.port());
+        inet_pton(AF_INET, deserialized_message.ip_address().c_str(), &dest_addr.sin_addr);
+
+        // Perform same sendto
+        // sendto()
+        n = recvfrom(sockfd, buffer, MAX_BUFFER_SIZE, 0, reinterpret_cast<sockaddr*>(&cliaddr), &len);
+        buffer[n] = '\0';
     }
 
     return 0;
